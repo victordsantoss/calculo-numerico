c9d9efa4e6c1eb3ae62f07b31980697e674fc6a7
Victor Samuel <victor.samuelsantoss@gmail.com>
2020-09-17 16:40:43 -0300

    Atualizaçao Metodo de Newton

--- metodo_de_newton_raphson.py
+++ metodo_de_newton_raphson.py
@@ -12,13 +12,13 @@ def newton_raphson ():
     x.append(chute_inicial)
 
     while True:
-        x.append (funcao_g(x[n]))
+        x.append (round (funcao_g(x[n]), 16))
         n += 1
-        print (f'x{n}: {x[n]:.20f} Erro: {abs(x[n]-x[n-1]):.20f}')
+        print (f'x{n}: {x[n]:<20.16f}  Erro: {abs(x[n]-x[n-1]):<20.16f}')
 
         if x[n] == x[n-1]:
             print (f'A soluçaõ convergiu corretamente na interação {n}')
-            print (f'Ponto fixo da equação: {x[n]:.15f}')
+            print (f'Ponto fixo da equação: {x[n]:<20.16f}')
             break;
 
     i = [i for i in range (1, len(x))]
@@ -31,13 +31,13 @@ def newton_raphson ():
     plt.show ()
     
 def funcao_g (x):
-    return x - (funcao(x)/derivada(x))
+    return round (x - (funcao(x)/derivada(x)), 16)
 
 def funcao (x):
-    return x**3-x-1
+    return round (x**3-x-1, 16)
 
 def derivada (x):
-    return 3*x**2-1
+    return round (3*x**2-1, 16)
 
 if __name__ == "__main__":
     newton_raphson ()
